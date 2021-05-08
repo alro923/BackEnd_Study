9d6387730a544157e85a632e69049713d20dd9e6
uhhyunjoo <alro923@naver.com>
2021-05-09 02:41:03 +0900

    🔨 Chapter 4 Viewer with mustache

--- src/main/java/dev/uhhyunjoo/service/posts/PostsService.java
+++ src/main/java/dev/uhhyunjoo/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package dev.uhhyunjoo.service.posts;
 
 import dev.uhhyunjoo.domain.posts.Posts;
 import dev.uhhyunjoo.domain.posts.PostsRepository;
+import dev.uhhyunjoo.web.dto.PostsListResponseDto;
 import dev.uhhyunjoo.web.dto.PostsResponseDto;
 import dev.uhhyunjoo.web.dto.PostsSaveRequestDto;
 import dev.uhhyunjoo.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,18 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(
                 ()->new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // readOnly : 등록/수정/삭제 없이 조회만 있는 서비스 메소드에 쓰면 속도 빨라짐!
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream() // postsRepository 의 결과로 넘어온 Posts 의 Stream 을
+                .map(PostsListResponseDto::new) // map 을 통해 PostsListResponseDto 로 변환해서 list 로 반환하는 메소드
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/dev/uhhyunjoo/web/IndexController.java
+++ src/main/java/dev/uhhyunjoo/web/IndexController.java
@@ -1,12 +1,38 @@
 package dev.uhhyunjoo.web;
 
+import dev.uhhyunjoo.service.posts.PostsService;
+import dev.uhhyunjoo.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+        // 앞 경로 : src/main/resources/templates
+        // 뒤의 파일 확장자 : .mustache 가 붙는다.
+        // '/' 를 호출하면 src/main/resources/templates/index.mustache 로 전환되어 View Resolver 가 처리
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+        // '/posts/save' 를 호출하면 posts-save.mustache 를 호출하는 메소드
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
